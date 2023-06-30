6eb69d8e529aee9a06319c2d497a97d6b2dac947
jd89j12dsa <138116539+jd89j12dsa@users.noreply.github.com>
2023-06-30 01:38:51 -0700

    Delete MITRAPP.sh

--- Scheme_MITRAPP/MITRAPP.sh
+++
@@ -1,5 +0,0 @@
-#python3.6 MITRAPP_test_mongomysql.py ToyDDSECrime bcds 2 0
-# python3.6 MITRAPP_test_mongomysql.py DSEWiki bcds 521 0
-python3.6 MITRAPP_test_mongomysql.py DSEWiki bcds 629 0 
-#python3.6 MITRAPP_test_mongomysql.py DDSECrimeC bcds 629 0 23.87
-#python3.6 MITRAPP_test_mongomysql.py DDSE2022VAERSVAXC bcds 629 0 
