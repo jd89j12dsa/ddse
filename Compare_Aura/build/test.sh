48e0b4a22c4f8ed89253e10b4828a6e9f6ea46bd
jd89j12dsa <138116539+jd89j12dsa@users.noreply.github.com>
2024-03-03 15:41:11 +0800

    Delete Compare_Aura directory

--- Compare_Aura/build/test.sh
+++
@@ -1,6 +0,0 @@
-python3.6 Aura_plan.py Crime_USENIX
-mkdir ./Result/
-sleep 10s
-./AuraServer &
-./AuraClient Crime_USENIXDB Crime_USENIXSearch Crime_USENIXREV> ./Result/Crime_USENIXDB00
-pkill AuraServer
