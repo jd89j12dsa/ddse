76ed8e44a58d03f71b383e909c21666452395ed7
jd89j12dsa <138116539+jd89j12dsa@users.noreply.github.com>
2024-03-03 15:42:08 +0800

    Delete Simulate_Seal_in_python directory

--- Simulate_Seal_in_python/test_Oidct.py
+++
@@ -1,7 +0,0 @@
-import OMAP
-OMAP.Setup(1,10)
-OMAP.Setup(30,10)
-OMAP.Ins(1,"1","5")
-OMAP.Ins(30,"2","10")
-print(OMAP.Find(1,"1"))
-print(OMAP.Find(30,"2"))
--- Simulate_Seal_in_python/test_Seal.sh
+++
@@ -1,1 +0,0 @@
-python3.6 Qplan_Seal.py Crime_USENIX 701 20 4
