ec2ece3b5374c9f9b3fac4499ba02315e30928c7
jd89j12dsa <138116539+jd89j12dsa@users.noreply.github.com>
2024-06-14 18:25:51 +0800

    Update test.sh

--- Compare_AURAp/build/test.sh
+++ Compare_AURAp/build/test.sh
@@ -1,5 +1,7 @@
 script_path=$(cd "$(dirname "$0")" && pwd)
 
+mkdir result
+
 $script_path/AuraServer &
 $script_path/AuraClient $script_path/dataset/Crime_USENIX_REV 0 0 > $script_path/result/Crime_USENIX_REV0
 pkill AuraServer
