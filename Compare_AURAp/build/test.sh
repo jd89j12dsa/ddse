3a666cde320ce3be4620c2652af5e93a8eaa8fa4
jd89j12dsa <138116539+jd89j12dsa@users.noreply.github.com>
2024-06-14 18:26:10 +0800

    Update test.sh

--- Compare_AURAp/build/test.sh
+++ Compare_AURAp/build/test.sh
@@ -3,7 +3,7 @@ script_path=$(cd "$(dirname "$0")" && pwd)
 mkdir result
 
 $script_path/AuraServer &
-$script_path/AuraClient $script_path/dataset/Crime_USENIX_REV 0 0 > $script_path/result/Crime_USENIX_REV0
+$script_path/AuraClient $script_path/dataset/Enron_USENIX 0 0 > $script_path/result/Enron_USENIX
 pkill AuraServer
 
 
